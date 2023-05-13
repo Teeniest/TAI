34d6af0024d294dd1e78140f3d6f4ffdbdcc51fb
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-13 14:02:01 -0600

    Create Notas.sql

---
+++ SQL/Notas.sql
@@ -0,0 +1,10 @@
+--Notas sobre SQL
+
+--inherits(tabla), permite la herencia a partir de una tabla dada, ejemplo:
+--create table puesto(
+--	posicion text
+--)inherits (tabla);
+--...........................................
+
+--CHAR. Para almacenar cadenas de caracteres de longitud fija, ejemplo: genero(M/F)
+--VARCHAR. Para almacenar cadenas de caracteres de longitud variable, ejemplo: direccion
