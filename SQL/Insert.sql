20ccf699068cb37fa5432616bb69cf94e516c213
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-13 14:27:13 -0600

    Update Insert.sql

--- SQL/Insert.sql
+++ SQL/Insert.sql
@@ -1,5 +1,5 @@
-insert into Fabricantes values ('TOVA990101NI5', Andres Torres Vargas);
---RFC char(13) not null,
+insert into Fabricantes values ('TOVA990101NI5', 'Andres Torres Vargas', '20 de noviembre, #47, Flores del valle, Boca del Rio, Veracruz, Mexico', 2290435178);
+--RFC char(13) not null 
 --Nom char not null,
 --Dir char not null,
 --Tel int not null,	
