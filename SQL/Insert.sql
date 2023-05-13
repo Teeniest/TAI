a5312c326902e4bc5389540b699bbd059893a248
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-13 14:20:09 -0600

    Update Insert.sql

--- SQL/Insert.sql
+++ SQL/Insert.sql
@@ -1,1 +1,5 @@
-insert into departamento values ('Alquimia', 1);
+insert into Fabricantes values ('TOVA990101NI5', Andres Torres Vargas);
+--RFC char(13) not null,
+--Nom char not null,
+--Dir char not null,
+--Tel int not null,	
