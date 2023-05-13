0983e295afaa59989ffc5426837114ec6f120e2f
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-13 16:28:42 -0600

    Ajuste de la longitud de variables

--- SQL/Tablas.sql
+++ SQL/Tablas.sql
@@ -9,59 +9,59 @@ create table Fabricante(
 	--Declaracion de columnas
 	RFC char(13) not null, 
 	Nom varchar(50) not null,
-	Dir varchar(50) not null,
+	Dir varchar(150) not null,
 	Tel bigint not null,
 	--Llaves primarias
 	constraint pk_fabricante primary key (RFC)
 );
 create table Piezas(
-	ID_pza char not null,
-	Lin char not null,
-	Color char not null,
+	ID_pza char(10) not null, --Se considera alfanumerico de 10 caract
+	Lin int not null,	  --Se considera numerico de 6 caract
+	Color varchar(20) not null,  --Nombre completo del color
 	--Llaves primarias
 	constraint pk_piezas primary key (ID_pza)
 );
 create table Armadura(
-	No_serie char not null,
+	No_serie char(10) not null,--Se considera alfanumerico de 10 caract
 	--Llaves primarias
 	constraint pk_armadura primary key (No_serie)
 );
 create table Distribuidor(
-	RFC_distr char not null,
-	Tel int not null,
-	Dir char not null,
-	Nom char not null,
+	RFC_distr char(13) not null,
+	Tel bigint not null,
+	Dir varchar(150) not null,
+	Nom varchar(50) not null,
 	--Llaves primarias
 	constraint pk_distribuidor primary key (RFC_distr)
 );
 create table Ingeniero(
-	ID_ing char not null,
-	Ced char not null,
-	GEM char not null,
-	Nom char not null,
-	Tel int not null,
+	ID_ing char(12)) not null, --Se considera alfanumerico de 12 caract
+	Ced char(10) not null,
+	GEM varchar(20) not null,	  --Nombre completo del grado
+	Nom varchar(50) not null,
+	Tel bigint not null,
 	--Llaves primarias
 	constraint pk_ingeniero primary key (ID_ing)
 );
 create table Cliente(
-	RFC char not null,
-	Dir char not null,
-	Tel int not null,
-	Nom char not null,
+	RFC char(13) not null,
+	Dir varchar(150) not null,
+	Tel bigint not null,
+	Nom varchar(50) not null,
 	--Llaves primarias
 	constraint pk_cliente primary key (RFC)
 );
 
 create table Categoria (
-	ID_cat char,
-	Nom char,
-	Info char
+	ID_cat char(10),	--Se considera alfanumerico de 10 caract
+	Nom varchar(9),	    --Categorias: pzgrande,pzachica,lowerbody,upperbody(Maximo 9 caracteres)
+	Info varchar(5)		--lower body: Altura y upperbody: tipo(brazo, pecho,casco)
 );
 create table Dimensiones (
-	ID_pza char,
-	Ancho decimal,
-	Alto decimal,
-	Largo decimal
+	ID_pza char(10),	--Se considera alfanumerico de 10 caract
+	Ancho decimal(4,4),
+	Alto decimal(4,4),
+	Largo decimal(4,4)
 );
 --Tablas que se desprenden de una relacion entre 2 tablas 
 create table Pie_Arm (
@@ -72,7 +72,7 @@ create table Arm_Ing (
 	CAF int,
 );
 create table Dis_Arm (
-	ID_vta char,
+	ID_vta char(10),	--Se considera alfanumerico de 10 caract
 	F_Vta date,
 	constraint pk_Dis_Arm primary key (ID_vta)
 );
