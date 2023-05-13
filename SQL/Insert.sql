68d5a0315814178ec209b5bda5888caf4c59107b
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-13 16:48:06 -0600

    Insertar los 5 datos en fabricantes

--- SQL/Insert.sql
+++ SQL/Insert.sql
@@ -1,5 +1,13 @@
-insert into Fabricante values ('TOVA990101NI5', 'Andres Torres Vargas', '20 de noviembre, #47,Flores del valle, 94200,Boca del Rio, Veracruz, Mexico', 2290435178);
+--Fabricante
+
+--Orden para ingresar los datos
 --RFC char(13) not null 
 --Nom char not null,
 --Dir char not null,
 --Tel int not null,	
+
+insert into Fabricante values ('TOVA990101NI5', 'Andres Torres Vargas', '20 de noviembre, #47,Flores del valle, 94200,Boca del Rio, Veracruz, Mexico', 2290435178);
+insert into Fabricante values ('VEVS990102NI5', 'Sandro Velasquez Vargas', '20 de noviembre, #47,Flores del valle, 94200,Boca del Rio, Veracruz, Mexico', 2290435178);
+insert into Fabricante values ('GUGA990104NI5', 'Alexandra Guillen Garcia', '20 de noviembre, #47,Flores del valle, 94200,Boca del Rio, Veracruz, Mexico', 2290435178);
+insert into Fabricante values ('GAFL990105NI5', 'Luis Garcia Flores', '20 de noviembre, #47,Flores del valle, 94200,Boca del Rio, Veracruz, Mexico', 2290435178);
+insert into Fabricante values ('GAAL990107NI5', 'Luis Roberto Garcia Aspe', '20 de noviembre, #47,Flores del valle, 94200,Boca del Rio, Veracruz, Mexico', 2290435178);
