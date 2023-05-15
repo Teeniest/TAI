2831db349eebd9c8f7df3ae02d1b11e7b695c6df
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-14 21:13:15 -0600

    1.9

--- CodingBat/CodingBatCaballeroAg.java
+++ CodingBat/CodingBatCaballeroAg.java
@@ -124,3 +124,20 @@ public boolean posNeg(int a, int b, boolean negative) {
 	return false;	 
 }
 
+/*
+1.9 Given a string, return a new string where "not " has been added to the front. However, 
+if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+*/
+public String notString(String str) {
+  String not="not ";
+  int t=str.indexOf("not");
+  if(t!=0){
+    str=not+str;
+    return str;
+  }
+  return str;
+}
