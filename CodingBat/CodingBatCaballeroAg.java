d1aaf861018389ad177450b2f05d4312dff7f781
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-14 20:45:01 -0600

    falta 1.4

--- CodingBat/CodingBatCaballeroAg.java
+++ CodingBat/CodingBatCaballeroAg.java
@@ -1,7 +1,6 @@
 //_______________________________________________________________WARMUP-1_______________________________________________________________
-/*SleepIn
-
-The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+/*
+1.1 The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
  We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
 
 sleepIn(false, false) → true
@@ -15,7 +14,7 @@ public boolean sleepIn(boolean weekday, boolean vacation) {
   return false;
 }
 /*
-We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. 
+1.2 We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. 
 We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
 
 monkeyTrouble(true, true) → true
@@ -29,7 +28,7 @@ public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
 	return false;
 }
 /*
-Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+1.3 Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
 sumDouble(1, 2) → 3
 sumDouble(3, 2) → 5
@@ -40,4 +39,20 @@ public int sumDouble(int a, int b) {
   	return 2*(a+b);
   }
   return a+b;
+}
+/*
+1.4 Given an int n, return the absolute difference between n and 21, except return double the absolute 
+difference if n is over 21.
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+*/
+public int diff21(int n) {
+  int res=n-21;
+  if(res<0){
+  	res=res*-1;
+  	return res;
+  }
+
 }
