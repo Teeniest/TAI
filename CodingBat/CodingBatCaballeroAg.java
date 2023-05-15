4de25d6afaa0839753cebaf616b6fb8c7f3caac6
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-14 20:40:23 -0600

    2

--- CodingBat/CodingBatCaballeroAg.java
+++ CodingBat/CodingBatCaballeroAg.java
@@ -14,3 +14,17 @@ public boolean sleepIn(boolean weekday, boolean vacation) {
   }
   return false;
 }
+/*
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. 
+We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false
+*/
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+	if(aSmile==true && bSmile==true){
+		return true;
+	}  
+	return false;
+}
