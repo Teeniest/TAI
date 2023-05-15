06e2c3d464b0af8f56ecbd5607486ac233ebb05a
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-05-14 20:42:43 -0600

    3

--- CodingBat/CodingBatCaballeroAg.java
+++ CodingBat/CodingBatCaballeroAg.java
@@ -23,8 +23,21 @@ monkeyTrouble(false, false) → true
 monkeyTrouble(true, false) → false
 */
 public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-	if(aSmile==true && bSmile==true){
+	if(aSmile==true && bSmile==true || (aSmile==false && bSmile==false)){
 		return true;
 	}  
 	return false;
+}
+/*
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+*/
+public int sumDouble(int a, int b) {
+  if(a==b){
+  	return 2*(a+b);
+  }
+  return a+b;
 }
