65bb33b21c2b18794bdc5889dade14fa4714ded4
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-06-08 16:35:31 -0600

    Distribuidores

--- SQL/Insert.sql
+++ SQL/Insert.sql
@@ -6,7 +6,7 @@
 --Dir char not null, Calle, numero, colonia, codigo postal ,estado, ciudad, pais
 --Tel int not null,	
 
-insert into Fabricante values ('TOVA9901011SA', 'Andres Torres Vargas', '20 de noviembre, #57,Flores del valle, 32141, Monterrey, Nuevo Leon, Mexico', 3574598161);
+
 insert into Fabricante values ('VEVS980402DU8', 'Sandro Velasquez Vargas', 'Rio ipanema,Capistrano, #103,Chalco, 86115, Nuevo Laredo, Tamaulipas, Mexico', 7492050849);
 insert into Fabricante values ('GUGA9405040WU', 'Alexandra Guillen Garcia', 'Sanchez Taboada, #504,Centro, 42933, San Luis Potosi, San Luis Potosi, Mexico', 6355734763);
 insert into Fabricante values ('GAFL040805NA0', 'Luis Garcia Flores', 'Alvaro Obregon, #4,Roma, 56562,Boca del Rio, Veracruz, Mexico', 8584976800);
@@ -19,16 +19,44 @@ insert into Fabricante values ('HETL850904XU7', 'Luis Roberto Hernandez Torres',
 
 
 --Piezas (id_pza, linea, color)
-insert into Piezas values('001','Azul')
-insert into Piezas values('001','Rojo')
-insert into Piezas values('002','Rojo')
-insert into Piezas values('002','Verde')
-insert into Piezas values('003','Negro')
-insert into Piezas values('002','Naranja')
-insert into Piezas values('005','Amarillo')
-insert into Piezas values('003','Rosa')
-insert into Piezas values('004','Blanco')
-insert into Piezas values('001','Negro')
+insert into Piezas values('001','Azul');
+insert into Piezas values('001','Rojo');
+insert into Piezas values('002','Rojo');
+insert into Piezas values('002','Verde');
+insert into Piezas values('003','Negro');
+insert into Piezas values('002','Naranja');
+insert into Piezas values('005','Amarillo');
+insert into Piezas values('003','Rosa');
+insert into Piezas values('004','Blanco');
+insert into Piezas values('001','Negro');
+
+--Distribuidor (Nombre, RFC, tel, Direccion)
+insert into Distribuidor values ('Piezas Roco', 'FAYF6890728CH',4687649166, 'Calo, #57,Flores del valle, 17445, Tulum, Quintana Roo, Mexico');
+insert into Distribuidor values ('Jaluwaqa', 'VAPS0564242KÑ',9854377004, 'Boca paila, #57,Flores del valle, 56747, Cuatitlan Izcalli, Ciudad de Mexico, Mexico');
+insert into Distribuidor values ('Componenetes Wido', 'ZAWJ9261499CT',4796029869, 'Roca partida, #101,Juarez, 10860, Villa union, Durango, Mexico');
+insert into Distribuidor values ('Nobaru electronics', 'TAZF3459021FQ',8865551473, 'Hacienda dos, #57,Periferico sur, 88714, San Cristobal de las casas, Chiapas, Mexico');
+insert into Distribuidor values ('Xano', 'QAXK7724552JB',2316357953, 'Sierra vieja, #57,Parroquia, 09327, Benito Juarez, Ciudad de Mexico, Mexico');
+insert into Distribuidor values ('PEZA', 'GIFT1902753YN',5397816071, 'Parque 4, #57,Fraccionamiento hacienda , 27245, San Francisco, Campeche, Mexico');
+insert into Distribuidor values ('Nebowi', 'XOVL1451699KQ',7089911367, 'Lindavista, #57,Carguero 3, 86954, Zapotlanejo, Jalisco, Mexico');
+insert into Distribuidor values ('QUEWO', 'MUQF1675506DC',7031633413, 'Cuenca dos, #57,Volcan, 77537, Puerto Vallarta, Jalisco, Mexico');
+insert into Distribuidor values ('Vake', 'FUÑR1772079VJ',6128717766, 'Via libre, #57,Conejo, 24788, Playa del Carmen, Quintana Roo, Mexico');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
