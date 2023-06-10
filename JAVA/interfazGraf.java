cebe8c628b420305d700ca08abb004879d722a0e
Teeniest <120422510+Teeniest@users.noreply.github.com>
2023-06-09 20:28:20 -0600

    Modificando panel consulta

--- JAVA/interfazGraf.java
+++ JAVA/interfazGraf.java
@@ -4,14 +4,19 @@ import java.awt.*;
 import javax.swing.*;
 
 public class interfazGraf {
+	BaseDatos conexion =  new BaseDatos();
 	public interfazGraf() {   
 		//Creación de ventana
 		JFrame ventana =new JFrame();
 		//Creación de los paneles(consulta,alta,baja, mod)
 	    JPanel p1=new JPanel();   
-	    JPanel p2=new JPanel();  
+	    p1.setLayout(null);
+	    JPanel p2=new JPanel();
+	    p2.setLayout(null);
 	    JPanel p3=new JPanel();
+	    p3.setLayout(null);
 	    JPanel p4=new JPanel();
+	    p4.setLayout(null);
 	    //Creación del panel de pestañas
 	    JTabbedPane tp=new JTabbedPane();
 	    tp.setBounds(10,10,750,320);  
@@ -22,14 +27,14 @@ public class interfazGraf {
 	    tp.add("Modificación",p4);
 	    //Agregar labels
 	    JLabel lid=new JLabel("Búsqueda por ID");
-	    lid.setBounds(1,10,30,30);
+	    lid.setBounds(0,0,300,20);
         p1.add(lid);
-        JLabel lcol=new JLabel("Búsqueda por Color");
-	    lcol.setBounds(1,10,30,30);
-        p1.add(lcol);
+        //JLabel lcol=new JLabel("Búsqueda por Color");
+	    //lcol.setBounds(1,10,30,30);
+        //p1.add(lcol);
         //Agregar campos de texto
-        JTextField tf_id=new JTextField("         ");  
-        tf_id.setBounds(100,100, 200,30);
+        JTextField tf_id=new JTextField("");  
+        tf_id.setBounds(120,0, 120,20);
         p1.add(tf_id);
 	    //Config ventana
 	    ventana.add(tp);  
